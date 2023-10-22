bd9b42d0a755112a54e2ad86023b97ce13a3eab0
Lucas <lucasiglesias2704@gmail.com>
2023-10-21 23:17:14 -0300

    exercicios 1 e 2

--- Listas-de-Exercicios/Lista-2-Recursividade/exercicio-1-numeros-naturais.c
+++ Listas-de-Exercicios/Lista-2-Recursividade/exercicio-1-numeros-naturais.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+void natural_numbers_until_n(int);
+
 int main()
 {
+  int n;
+
+  scanf("%d", &n);
+
+  natural_numbers_until_n(n);
 
   return 0;
+}
+
+void natural_numbers_until_n(int n)
+{
+  if (n == 0)
+  {
+    return;
+  }
+  else
+  {
+    natural_numbers_until_n(n - 1);
+    printf("%d ", n);
+  }
 }
