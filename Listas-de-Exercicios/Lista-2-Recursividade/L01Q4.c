9720442ad3ba4bb01e4171045923da45f058ab4f
Lucas <lucasiglesias2704@gmail.com>
2023-11-03 00:21:59 -0300

    exercicio 4 feito grazadeus

--- Listas-de-Exercicios/Lista-2-Recursividade/L01Q4.c
+++ Listas-de-Exercicios/Lista-2-Recursividade/L01Q4.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <math.h>
+
+int reverse(int, int);
 
 int main()
 {
+  int n;
+
+  scanf("%d", &n);
+
+  printf("%d", reverse(n, 0));
 
   return 0;
+}
+
+int reverse(int n, int revNum) 
+{
+  if (n == 0)
+  {
+    return revNum;
+  }
+
+  revNum = (revNum * 10) + (n % 10);
+
+  return reverse(n / 10, revNum);
 }
